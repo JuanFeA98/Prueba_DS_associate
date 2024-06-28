6edd8ccadfb5eddd7ba4393370c77d97434fc1d7
JuanFe <jmartinezbernal02@gmail.com>
2024-06-28 09:04:27 -0500

    Version final

--- Utils/functions.py
+++ Utils/functions.py
@@ -1,38 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import logging
-# import requests
-
-def config_logging(dia, append=True):
-    # Especificamos el formato del logging
-    LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-
-    # Definimos el modo de escritura
-    filemode = 'a' if append == True else 'w'
-
-    # Configuramos nuestro logging
-    logging.basicConfig(
-        filename=f'./Log/log_{dia}.txt',
-        level=logging.INFO,
-        format=LOG_FORMAT,
-        filemode=filemode
-    )
-
-    # Inicializamos el logging
-    logger = logging.getLogger()
-
-    return logger
-
-# def send_message(id, message):
-#     # Ajustamos la solicitud de envio
-#     result = requests.post(
-#         id, 
-#         json={
-#             'text': message,
-#         }
-#     )
-
 # Función para formatear los valores del eje x
 def thousands_formatter(x, pos):
     return '{:,.0f}'.format(x / 1000000)
